1e39fb38548ad23ad1084686215fe06c38a5b867
hafza-mughal <156007447+hafza-mughal@users.noreply.github.com>
2024-04-08 20:45:49 +0500

    Add files via upload

---
+++ java2.js
@@ -0,0 +1,31 @@
+let grade=prompt("Enter your grade")
+
+
+if(grade>=90 && grade<=100){
+
+    console.log("Wonderful you got a higher marks")
+}
+else if(grade>=80 && grade<90){
+    console.log("Great Job")
+}
+else if(grade>=70 && grade<80){
+    console.log("Well Done")
+}
+else if(grade>=60 && grade <70){
+    console.log("Not Bad")
+}
+else if(grade>=50 && grade<60){
+    console.log("A piece of paper doesn't decide your future.Don't be sad")
+}
+else if(grade>=40 && grade<50){
+    console.log("Next Time")
+}
+else if(grade>=33 && grade<40){
+    console.log("You Need a Hard Work")
+}
+else if (grade>=1 && grade<33) {
+    console.log("Future entrepreneur")
+}
+else{
+    console.log("Enter Valid Number")
+};
